b4aac395d1b2b4adfa3a9e7c946689777d75f239
Dani Angeli <daniangelisilva@gmail.com>
2018-09-28 06:42:59 -0300

    Alowing to comment the articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :articles, only: [:index, :new, :create, :show, :destroy, :edit, :update]
+  resources :articles, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
+    resources :comments, only: [:new, :create]
+  end
+  resources :comments, only: [:show, :edit, :update, :destroy]
 end
